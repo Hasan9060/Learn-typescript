48524e13b86db59116878f65e4454b467817825b
Hasan9060 <160109106+Hasan9060@users.noreply.github.com>
2024-03-03 13:54:11 +0500

    Final-version

---
+++ 45 question solution/Question_05_FamousPerson/main.ts
@@ -0,0 +1,8 @@
+// Question 5:
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famousPerson : string = "Quaid e azam";
+
+let message : string = "once said,\" Knowledge is more powerful than the sword, so spread knowledge in your country. No one can defeat you.\""
+
+console.log(famousPerson, message);
---
+++ 45 question solution/Question_07 and 08_Only# Eight Answer/main.js
@@ -0,0 +1,10 @@
+// Question : 7
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// Question : 8
+// You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(4 + 4);
+console.log(12 - 4);
+console.log(2 * 4);
+console.log(16 / 2);
