6140a71f43218d0cc1d50d4b131a03da249428c7
Hasan9060 <160109106+Hasan9060@users.noreply.github.com>
2024-03-03 13:15:32 +0500

    asd

--- 45 Question Solution/Question_03_Name_case/main.js
+++
@@ -1,4 +0,0 @@
-let persoName = "Syed Hasan";
-console.log(persoName.toLowerCase());
-console.log(persoName.toUpperCase());
-console.log(persoName.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
